c7d19db6594fb4a98583101b9987849402921886
chirag-empiric <chirag@empiricinfotech.com>
2024-05-31 11:37:35 +0530

    refactor: custom port using .env

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,22 @@
+use dotenv;
+
 #[macro_use]
 extern crate rocket;
 pub mod api;
 mod db;
 
 use api::{create_user, delete_user, get_all_users, get_user, update_user};
+use rocket::Config;
 
 #[launch]
+
 async fn rocket() -> _ {
+    dotenv::dotenv().ok();
+
     let db = db::connect().await.unwrap().database("sample_mflix");
 
-    rocket::build()
+    let rocket_config = Config::figment();
+    rocket::custom(rocket_config)
         .mount(
             "/",
             routes![
